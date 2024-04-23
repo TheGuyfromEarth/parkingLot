f95f0b44a95aaf09cd22e869ab5eafc9aa57f8e7
Rishabh Gupta <rishabhgupta01994@gmail.com>
2024-04-23 10:54:08 +0530

    lab 9 and 10

---
+++ src/lab10/District.java
@@ -0,0 +1,137 @@
+package lab10;/*
+ * CS1011 - TODO
+ * Fall 2020
+ * Lab 10 - ParkingLots
+ * Name: TODO
+ * Created: 11/6/2019
+ */
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Manages parking lots within a district.
+ *
+ * @author [TODO: put your name here]
+ */
+public class District {
+    private List<ParkingLot> lots;
+    private int numLots;
+    private int closingTimestamp;
+    private int totMinsClosed;
+
+    private boolean flag = false;
+
+    /**
+     * Set up a district with three parking lots.
+     */
+    public District() {
+        lots = new ArrayList<>();
+    }
+
+    public int addLot(String name, int capacity) {
+        int newIndex = numLots;
+        lots.add(new ParkingLot(name, capacity));
+        numLots++;
+        return newIndex;
+    }
+
+    public ParkingLot getLot(int index) {
+        if (index >= 0 && index < lots.size()) {
+            return lots.get(index);
+        }
+        return null;
+    }
+
+    /**
+     * Returns the number of remaining parking spots in the district
+     *
+     * @return the number of remaining parking spots in the district
+     */
+    public int getNumberOfSpotsRemaining() {
+        // TODO: Complete this stub
+        int numSpotsRemaining = 0;
+        for (int i = 0; i < numLots; i++)
+            numSpotsRemaining += lots.get(i).getNumberOfSpotsRemaining();
+        return numSpotsRemaining;
+    }
+
+    /**
+     * Returns the amount of time all three lots have been
+     * simultaneously closed.
+     *
+     * @return number of minutes all three lots have been closed
+     */
+    public int getMinutesClosed() {
+        // TODO: Complete this stub
+        return totMinsClosed;
+    }
+
+    /**
+     * Checks the status of all three lots in the district and
+     * returns true if they are all closed and false otherwise.
+     *
+     * @return whether all three lots in the district are closed
+     */
+    public boolean isClosed() {
+        // TODO: Complete this stub
+        for (int i = 0; i < numLots; i++) {
+            if (!lots.get(i).isClosed())
+                return false;
+        }
+        return true;
+    }
+
+    /**
+     * Record a vehicle entering a lot at a specified timestamp.
+     * <p></p>
+     * This calls lab8.ParkingLot.markVehicleEntry for the lot corresponding
+     * to lotNumber (e.g., if lotNumber==1, call markVehicleEntry on lot1).
+     * <p></p>
+     * If lotNumber is out of range, the method should return without
+     * doing anything else.
+     *
+     * @param lotNumber Number of lot (should be 1, 2, or 3)
+     * @param timestamp Entry timestamp in minutes since all lots were opened.
+     */
+    public void markVehicleEntry(int lotNumber, int timestamp) {
+        // TODO: Complete this stub
+        lots.get(lotNumber).markVehicleEntry(timestamp);
+        if (isClosed() && !flag) {
+            closingTimestamp = timestamp;
+            flag = true;
+        }
+    }
+
+    /**
+     * Record a vehicle exiting a lot at a specified timestamp.
+     * <p></p>
+     * This calls lab8.ParkingLot.markVehicleExit for the lot corresponding
+     * to lotNumber (e.g., if lotNumber==1, call markVehicleExit on lot1).
+     * <p></p>
+     * If lotNumber is out of range, the method should return without
+     * doing anything else.
+     *
+     * @param lotNumber Number of lot (should be 1, 2, or 3)
+     * @param timestamp Exit timestamp in minutes since all lots were opened.
+     */
+    public void markVehicleExit(int lotNumber, int timestamp) {
+        // TODO: Complete this stub
+        lots.get(lotNumber).markVehicleExit(timestamp);
+        if (!isClosed() && flag) {
+            totMinsClosed += timestamp - closingTimestamp;
+            flag = false;
+        }
+    }
+
+    public String toString() {
+        String str = "";
+        str += "District status:\n";
+        for (int i = 0; i < numLots; i++) {
+            str += "  ";
+            str += lots.get(i).toString();
+            str += "\n";
+        }
+        return str;
+    }
+}
---
+++ src/lab10/ParkingLot.java
@@ -0,0 +1,80 @@
+package lab10;
+
+import java.text.DecimalFormat;
+
+public class ParkingLot {
+    public static final double CLOSED_THRESHOLD = 80.0;
+    private String name;
+    private int capacity;
+    private int numSlotsUsed;
+
+    private int closingTimestamp;
+    private int lastTimestamp;
+    private int totMinsClosed;
+
+    private boolean flag = false;
+
+    public ParkingLot(int capacity) {
+        this.capacity = capacity;
+        this.name = "test";
+    }
+
+    public ParkingLot(String name, int capacity) {
+        this.name = name;
+        this.capacity = capacity;
+    }
+
+    public int getMinutesClosed() {
+        return totMinsClosed;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getNumberOfSpotsRemaining() {
+        return capacity - numSlotsUsed;
+    }
+
+    public double getPercentFull() {
+        return (numSlotsUsed * 100.0 / capacity);
+    }
+
+    public boolean isClosed() {
+        return numSlotsUsed >= (CLOSED_THRESHOLD / 100) * capacity;
+    }
+
+    public void markVehicleEntry(int timestamp) {
+        if (timestamp >= lastTimestamp) {
+            numSlotsUsed++;
+            lastTimestamp = timestamp;
+            if (isClosed() && !flag) {
+                closingTimestamp = timestamp;
+                flag = true;
+            }
+        }
+    }
+
+    public void markVehicleExit(int timestamp) {
+        if (timestamp >= lastTimestamp) {
+            numSlotsUsed--;
+            lastTimestamp = timestamp;
+            if (!isClosed() && flag) {
+                totMinsClosed += timestamp - closingTimestamp;
+                flag = false;
+            }
+        }
+    }
+
+    public String toString() {
+        String percentFull = "";
+        if (isClosed())
+            percentFull = "CLOSED";
+        else {
+            DecimalFormat formatter = new DecimalFormat("#.#");
+            percentFull = formatter.format(getPercentFull())+"%";
+        }
+        return "Status for " + getName() + " parking lot: " + numSlotsUsed + " vehicles (" + percentFull + ")";
+    }
+
+}
